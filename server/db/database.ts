b9d17c63ec5a32aba7cbd6e955d4c48cf302698a
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-05-31 09:10:36 +1200

    added some database functions

--- server/db/database.ts
+++ server/db/database.ts
@@ -3,15 +3,18 @@ import { User } from '../../models/User.ts'
 import { Workout } from '../../models/Workouts.ts'
 
 export async function getAllUsers(): Promise<User[]> {
-  return connection('users').select()
+  return await connection('users').select()
 }
 
 export async function getUserById(id: number): Promise<User> {
-  return connection('users').where({id}).first()
+  const res = await connection('users').where({id}).first()
+  return res
 }
 
 export async function getUserWorkoutsById(id: number): Promise<Workout[]> {
-  const response = connection('workouts').join('users', 'workouts.user_id', 'users.id').where({id})
-  console.log(response)
-  return response
+  const res = await connection('workouts').where('workouts.user_id', id)
+  console.log(res)
+  return res
 }
+
+// getUserWorkoutsById(1)
--- server/db/seeds/workouts.js
+++ server/db/seeds/workouts.js
@@ -8,6 +8,7 @@ export const seed = async function(knex) {
   await knex('workouts').insert([
     {id: 1, user_id: 1, workout_name: 'back', date: '15/05', is_shared: false},
     {id: 2, user_id: 2, workout_name: 'chest', date: '15/05', is_shared: false},
-    {id: 3, user_id: 3, workout_name: 'legs', date: '15/05', is_shared: false}
+    {id: 3, user_id: 3, workout_name: 'legs', date: '15/05', is_shared: false},
+    {id: 4, user_id: 1, workout_name: 'chest', date: '15/05', is_shared: false},
   ]);
 };
