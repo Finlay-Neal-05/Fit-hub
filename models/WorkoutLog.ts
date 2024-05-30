a30d6d28786a5ab14384fbf1026b89476aaf82ef
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-05-31 11:50:49 +1200

    added function to log a workout

--- models/WorkoutLog.ts
+++ models/WorkoutLog.ts
@@ -3,7 +3,6 @@ import { ExerciseData } from "./Exercise";
 import { WorkoutData } from "./Workouts";
 
 export interface WorkoutLog {
-  user_id: number
   workout: WorkoutData
   sets: SetData[]
   exercises: ExerciseData[]
