ea0cd9c39b7888e588db43cb2ff1b1e9f756954f
ShaneSaw1705 <shanecurtis1705@gmail.com>
2024-05-30 21:08:16 +1200

    #

---
+++ server/db/migrations/20240530045320_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+import knex from "knex"
+
+export async function up() {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('password')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down() {
+  return knex.schema.dropTable('users')
+}
