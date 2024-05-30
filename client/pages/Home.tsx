6a43b6767f8e2a48d7c6f2a33d98db7b27e54da7
Andrew Molten <andrewmolten23@gmail.com>
2024-05-30 20:25:29 +1200

    create PreviousWorkout component

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,7 +1,13 @@
+import PreviousWorkout from '../components/PreviousWorkout'
+
 function Home() {
   return (
     <>
       <h2>Home</h2>
+
+      <PreviousWorkout />
+      <PreviousWorkout />
+      <PreviousWorkout />
     </>
   )
 }
