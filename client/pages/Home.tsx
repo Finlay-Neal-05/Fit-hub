0fe2bb6c00083403a33882cbcb5cf417f795fa9d
Andrew Molten <andrewmolten23@gmail.com>
2024-05-31 12:15:36 +1200

    something

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 // get the data from the backend with the workoutData
 
+// /api/v1/users/getworkoutsbyid/:id
+
 function Home() {
   const currDate = new Date().toDateString()
   return (
