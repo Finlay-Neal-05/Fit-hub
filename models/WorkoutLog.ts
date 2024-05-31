328684ec503cd0a192dae372fbaceaee8260e3ac
Finlay Neal <finlayhormann@gmail.com>
2024-05-31 14:11:33 +1200

    complete add set form

--- models/WorkoutLog.ts
+++ models/WorkoutLog.ts
@@ -1,8 +1,9 @@
 import { SetData } from "./Set";
 import { ExerciseData } from "./Exercise";
+import { WorkoutData } from "./Workouts";
 
 export interface WorkoutLog {
-  user_id: number
+  workout: WorkoutData
   sets: SetData[]
   exercises: ExerciseData[]
 }
